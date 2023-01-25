19dacd1e51ecab224c033de5e2c367a1f4e4c79c
Yomakota <89361386+Yomakota@users.noreply.github.com>
2023-01-25 16:47:18 +0100

    add gitignore

--- src/org/lessons/java/sondaggio/sondaggio.java
+++ src/org/lessons/java/sondaggio/sondaggio.java
@@ -11,29 +11,29 @@ public class sondaggio {
 		int studentsWindows;
 		int studentsLinux;
 
-		Scanner data= new Scanner(System.in);
+		Scanner s= new Scanner(System.in);
 
 		System.out.println("How many students in your class?");
-		students = data.nextInt();
+		students = s.nextInt();
 
 		System.out.println("How many students use MacOs?");
-		studentsMacOs = data.nextInt();
+		studentsMacOs = s.nextInt();
 
 		System.out.println("How many students use Windows?");
-		studentsWindows = data.nextInt();
+		studentsWindows = s.nextInt();
 
 		System.out.println("How many students use Linux?");
-		studentsLinux = data.nextInt();
+		studentsLinux = s.nextInt();
 
-		int UserStudentsMacOs = (studentsMacOs * 100) / students;
-		int UserStudentsWindows = (studentsWindows * 100) / students;
-		int UserStudentsLinux = (studentsLinux * 100) / students;
+		int UserStudentsMacOs = (int) (((double)studentsMacOs * 100) / students);
+		int UserStudentsWindows = (int) (((double)studentsWindows * 100) / students);
+		int UserStudentsLinux = (int) (((double)studentsLinux * 100) / students);
 
 		System.out.println(UserStudentsMacOs + "%" + " of students use MacOs");
 		System.out.println(UserStudentsWindows + "%" + " of students use Windows");
 		System.out.println(UserStudentsLinux + "%" + " of students use Linux");
 		
-		data.close();
+		s.close();
 	}
 
 }
