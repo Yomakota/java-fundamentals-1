dd09092457f6507f5501bec5dc467cedb6d0d3e7
Yomakota <89361386+Yomakota@users.noreply.github.com>
2023-01-26 15:29:56 +0100

    add last exercise

---
+++ src/org/lessons/java/Ferragnez/CheckGuest.java
@@ -0,0 +1,32 @@
+package org.lessons.java.Ferragnez;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+
+	public static void main(String[] args) {
+
+		String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+				"Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeili" };
+
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Insert your name: ");
+		
+		String Username = s.nextLine();
+
+		s.close();
+
+		for (int i = 0; i < guests.length; i++) {
+			
+			if (Username.toLowerCase().equals(guests[i].toLowerCase())) {
+				System.out.println("Welcome: " + Username);
+				break;
+				
+			} else {
+				System.out.println("You are not on the list!");
+				break;
+			}
+		}
+	}
+}
