0e7be3737381e0412dba20cba45082dd815d7f1e
Yomakota <89361386+Yomakota@users.noreply.github.com>
2023-01-26 14:54:24 +0100

    add favourite food

---
+++ src/org/lessons/java/favourite_food/Favourite_food.java
@@ -0,0 +1,17 @@
+package org.lessons.java.favourite_food;
+
+public class Favourite_food {
+
+	public static void main(String[] args) {
+
+		String[] FavouriteFood = {"pasta", "torta", "pesce", "carne", "cioccolato", "frutta"};
+		
+		System.out.println("Lunghezza classifica: " + FavouriteFood.length);
+		
+		System.out.println("cibo preferito : " + FavouriteFood[0]);
+		
+		System.out.println("meno preferito : " + FavouriteFood[5]);
+
+	}
+
+}
